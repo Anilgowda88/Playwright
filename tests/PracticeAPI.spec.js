5ae803f6fd65596a204c876f436749e8efe9a268
Anilgowda88 <91017230+Anilgowda88@users.noreply.github.com>
2025-11-28 16:52:42 -0500

    tags

--- tests/PracticeAPI.spec.js
+++ tests/PracticeAPI.spec.js
@@ -2,7 +2,7 @@ const {test, expect} = require ('@playwright/test');
 const { brotliDecompress } = require('zlib');
 require('dotenv').config({ override: true });
 
-test('Test API Get Posts',async ({request}) =>
+test('@API Test API Get Posts',async ({request}) =>
 {
     const response = await request.get('https://jsonplaceholder.typicode.com/posts')
     expect(response.status()).toBe(200);
@@ -12,7 +12,7 @@ test('Test API Get Posts',async ({request}) =>
     // expect(body.id).toBe(1);
 });
 
-test('Test API Get Users',async ({request}) =>
+test('@API Test API Get Users',async ({request}) =>
 {
     const response = await request.get('https://jsonplaceholder.typicode.com/users')
     expect(response.status()).toBe(200);
